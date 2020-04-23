739911f79daac899357886c56ac52e081dbe2982
Jared <JHCover@gmail.com>
2020-04-23 14:04:47 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,155 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, combineReducers } from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+
+const todo = (state, action) => {
+    switch(action.type) {
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            };
+        case 'TOGGLE_TODO':
+            if (state.id !== action.id) {
+                return state;
+            }
+            return {
+                ...state,
+                completed: !state.completed
+            }
+        default:
+            return state;
+    }
+};
+
+const todos = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [...state,
+                todo(undefined, action)];
+        case 'TOGGLE_TODO':
+            return state.map(t => todo(t, action));
+        default:
+            return state;
+    }
+}
+
+const visibilityFilter = (
+    state = 'SHOW_ALL',
+    action
+) => {
+    switch (action.type) {
+        case 'SET_VISIBILITY_FILTER':
+            return action.filter;
+        default:
+            return state;
+    }
+};
+
+// const todoApp = (state = {}, action) => {
+//     return {
+//         todos: todos(
+//             state.todos,
+//             action
+//         ),
+//         visibilityFilter: visibilityFilter(
+//             state.visibilityFilter,
+//             action
+//         )
+//     };
+// };
+
+const todoApp = combineReducers({
+    todos,
+    visibilityFilter
+});
+
+const store = createStore(todoApp);
+
+let nextTodoId = 0;
+class TodoApp extends Component {
+    render() {
+        return (
+            <div>
+                <button onClick={ () => {
+                    store.dispatch({
+                        type: 'ADD_TODO',
+                        text: 'Test',
+                        id: nextTodoId++
+                    });
+                }}>Add Todo
+                </button>
+                <ul>
+                    {this.props.todos.map(todo =>
+                        <li key={todo.id}>
+                            {todo.text}
+                        </li>
+                    )}
+                </ul>
+            </div>
+        )
+    }
+}
+
+
+const render = () => {
+    ReactDOM.render(
+        <TodoApp
+            todos={store.getState().todos}
+        />,
+        document.getElementById('root')
 );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+store.subscribe(render);
+render();
+
+console.log('Initial state:');
+console.log(store.getState());
+console.log('----------------');
+
+console.log('Dispatching ADD_TODO.');
+store.dispatch({
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux'
+});
+
+console.log('Current state: ');
+console.log(store.getState());
+console.log('----------------');
+
+console.log('Dispatching ADD_TODO.');
+store.dispatch({
+    type: 'ADD_TODO',
+    id: 1,
+    text: 'Go shopping'
+});
+
+console.log('Current state: ');
+console.log(store.getState());
+console.log('----------------');
+
+console.log('Dispatching TOGGLE_TODO');
+store.dispatch({
+    type: 'TOGGLE_TODO',
+    id: 0
+});
+
+console.log('Current state: ');
+console.log(store.getState());
+console.log('----------------');
+
+console.log('Dispatching TOGGLE_VISIBILITY_FILTER.');
+store.dispatch({
+    type: 'SET_VISIBILITY_FILTER',
+    filter: 'SHOW_COMPLETED'
+});
+
+console.log('Current state: ');
+console.log(store.getState());
+console.log('----------------');
