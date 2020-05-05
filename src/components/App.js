854906f50f23b82f02ac693d41c7d04a2fd7a283
Jared Cover <jhcover@gmail.com>
2020-05-05 10:41:34 -0700

    ready for step 7

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import AddTodo from "./AddTodo";
 import Footer from "./Footer";
 import React from "react";
-import {Provider} from "react-redux";
-import configureStore from '../configureStore';
 import VisibleTodoList from "./VisibleTodoList";
 
 const App = () => (
