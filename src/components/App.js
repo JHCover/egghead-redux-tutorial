825c8e4287bed6bbb04265f08d5235252841ec34
Jared Cover <jhcover@gmail.com>
2020-05-05 11:06:53 -0700

    configure react-router to be the source of truth for the selected visibility filter

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,23 @@ import AddTodo from "./AddTodo";
 import Footer from "./Footer";
 import React from "react";
 import VisibleTodoList from "./VisibleTodoList";
+import { useParams } from "react-router-dom";
 
-const App = () => (
-    <div>
-        <AddTodo/>
-        <VisibleTodoList/>
-        <Footer/>
-    </div>
-);
+
+const App = () => {
+
+    let {filter} = useParams();
+
+    return (
+        <div>
+            <AddTodo/>
+            <VisibleTodoList
+                filter={filter || 'all'}
+            />
+            <Footer/>
+        </div>
+    );
+}
 
 export default App;
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,9 @@
 import {todos} from './todos'
-import {visibilityFilter} from "./visibilityFilter";
 import {combineReducers} from "redux";
 
 const rootReducer = combineReducers({
     todos,
-    visibilityFilter
+
 });
 
 export default rootReducer;
